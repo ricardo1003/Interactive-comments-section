ed495924520ba029f6c122f6624be30bc4b6d01b
unknown <ricardodonado1105@gmail.com>
2024-10-13 17:55:53 -0500

    addded 'comment' component with the inner score div as another component(score.jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import {Comment} from "./components/comment"
+import { comments as data } from "./data/data.json";
+
 
 function App() {
   return (
     <>
-      
+      <main className="flex flex-col max-w-[750px] mx-auto gap-4">
+        {data.map((comment)=>(
+          <Comment
+            id={comment.id}
+            key={comment.id}
+            author={comment.user.username}
+            content={comment.content}
+            profilePicture={comment.user.image.png}
+            createdAt={comment.createdAt}
+            score={comment.score}
+          ></Comment>
+        ))}
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ModerateBlue: "hsl(238, 40%, 52%)",
+        SoftRed: "hsl(358, 79%, 66%)",
+        LightGrayishBlue: "hsl(239, 57%, 85%)",
+        PaleRed: "hsl(357, 100%, 86%)",
+
+        DarkBlue: "hsl(212, 24%, 26%)",
+        GrayishBlue: "hsl(211, 10%, 45%)",
+        LightGray:" hsl(223, 19%, 93%)",
+        VeryLightGray: "hsl(228, 33%, 97%)",
+      },
+      backgroundImage: {
+        plusButton: "../src/assets/images/icon-plus.svg",
+        minusBUtton: "../src/assets/images/icon-minus.svg",
+      }
+    },
   },
   plugins: [],
 }
