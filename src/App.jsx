a86dec07851a127d973d07e1f5b487a54846d029
unknown <ricardodonado1105@gmail.com>
2024-10-18 13:35:01 -0500

    succesfully created the replies

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,38 @@ import { Fragment, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import {Comment} from "./components/comment"
+import { Comment } from "./components/comment";
 import { comments as data } from "./data/data.json";
-
 
 function App() {
   return (
     <>
       <main className="flex flex-col max-w-[750px] mx-auto gap-4">
-        {data.map((comment)=>(
+        {data.map((comment) => (
           <Comment
-            id={comment.id}
+            hasComments={comment.replies.length ? true : false}
+            id={`comment#${comment.id}`}
             key={comment.id}
             author={comment.user.username}
             content={comment.content}
             profilePicture={comment.user.image.png}
             createdAt={comment.createdAt}
             score={comment.score}
-          ></Comment>
+          >
+            {comment.replies.map((reply) => (
+               <Comment
+               key={reply.id}
+                 id={`reply#${reply.id}`}
+                 author={reply.user.username}
+                 content={reply.content}
+                 profilePicture={reply.user.image.png}
+                 createdAt={reply.createdAt}
+                 score={reply.score}
+                 >
+                 {console.log(reply)}
+               </Comment>
+            ))}
+          </Comment>
         ))}
       </main>
     </>
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,4 +1,5 @@
 import { Score } from "./Score";
+import { Response } from "./Response";
 
 export function Comment({
   profilePicture,
@@ -6,27 +7,37 @@ export function Comment({
   createdAt,
   score,
   content,
-  id
+  id,
+  children,
+  hasComments,
 }) {
   return (
-    <section className="bg-white rounded-xl p-6 flex">
-      <Score score={score} id={id} />
-      <div className="mainInfo flex flex-col gap-2">
-        <div className="user flex gap-2">
-          <img
-            src={profilePicture}
-            alt={author}
-            className="w-8 aspect-square block"
-          />
-          <h3 className="font-bold text-DarkBlue">{author}</h3>
-          <p className="text-GrayishBlue">{createdAt}</p>
-          <button className="flex text-ModerateBlue ml-auto items-center gap-2 font-bold">
-            <img src="../src/assets/images/icon-reply.svg" alt="reply" />
-            Reply
-          </button>
+    <section className="flex flex-col gap-4">
+      <section className="bg-white rounded-xl p-6 flex">
+        <Score score={score} id={id} />
+        <div className="mainInfo flex flex-col gap-2">
+          <div className="user flex gap-2">
+            <img
+              src={profilePicture}
+              alt={author}
+              className="w-8 aspect-square block"
+            />
+            <h3 className="font-bold text-DarkBlue">{author}</h3>
+            <p className="text-GrayishBlue">{createdAt}</p>
+            <button className="flex text-ModerateBlue ml-auto items-center gap-2 font-bold hover:opacity-50">
+              <img src="../src/assets/images/icon-reply.svg" alt="reply" />
+              Reply
+            </button>
+          </div>
+          <p className="text-GrayishBlue">{content}</p>
         </div>
-        <p className="text-GrayishBlue">{content}</p>
-      </div>
+      </section>
+      {hasComments ? (<div className="flex flex-row">
+        <div className="self-stretch w-[2px] bg-black/10 block mx-8"></div>
+        <article className="flex flex-col gap-4 w-[100%]">
+          {children}
+        </article>
+      </div>) : <></>}
     </section>
   );
 }
